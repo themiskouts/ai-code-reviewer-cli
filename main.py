9de4edb04ac7fe528c24963186b404e86b603029
Themistoklis-Thomas Koutsoukis <ithemhsgr@gmail.com>
2026-08-05 14:42:03 +0300

    feat: complete base project setup

---
+++ main.py
@@ -0,0 +1,9 @@
+import sys
+
+def main():
+    print("==================================")
+    print("   AI Code Reviewer CLI Started   ")
+    print("==================================")
+
+if __name__ =="__main__":
+    main()
